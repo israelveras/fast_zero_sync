c1a4ce9c68dee226293b04fd44797cd210924753
Israel Veras <israelveras@gmail.com>
2024-07-04 08:25:57 -0300

    v2

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,82 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi import (
+    FastAPI,  # , Request, Response
+)
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
+templates = Jinja2Templates(directory='fast_zero/templates')
 
 app = FastAPI()
 
 
-@app.get('/')
+# Middleware para registrar os cabeçalhos de requisição e resposta
+# @app.middleware("http")
+# async def log_request_response(request: Request, call_next):
+#     # Log dos cabeçalhos da requisição
+#     print("Requisição:")
+#     for header, value in request.headers.items():
+#         print(f"{header}: {value}")
+
+#     # Chamar o próximo middleware ou manipulador de rota
+#     response = await call_next(request)
+
+#     # Log dos cabeçalhos da resposta
+#     print("Resposta:")
+#     for header, value in response.headers.items():
+#         print(f"{header}: {value}")
+
+#     print("-"*100)
+#     print(request.headers)
+#     print("-"*100)
+#     print(response.headers)
+#     print("-"*100)
+
+#     return response
+
+data_base = []
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo todo!'}
+
+
+@app.post(
+    '/users/',
+    status_code=HTTPStatus.CREATED,
+    response_model=UserPublic,
+    response_class=JSONResponse,
+)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(data_base) + 1, **user.model_dump())
+    data_base.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_user():
+    return {'users': data_base}
+
+
+@app.put('/users/', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    user = UserDB(id=len(data_base) + 1, **user.model_dump())
+    print(data_base)
+    data_base.append(user)
+    return user
+
+@app.get('/index', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse('ola_mundo.html', {'request': request})
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host='127.0.0.1', port=8000)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,44 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo todo!'}
+
+
+# def test_retorna_html_em_index(client):
+#     response = client.get('/index')
+#     assert response.status_code == HTTPStatus.OK
+#     assert response.headers['content-type'] == 'text/html; charset=utf-8'
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'test',
+            'password': 'password',
+            'email': 'test@test.com',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'test',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+
+def test_get_list_user(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'test',
+                'email': 'test@test.com',
+                'id': 1,
+            }
+        ]
+    }
